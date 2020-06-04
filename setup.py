febd8bd4bfd353df56812f064b4ecbd48d5c3442
Matthew Reschke <mail@mreschke.com>
2020-06-04 11:49:38 -0600

    Updates

--- setup.py
+++ setup.py
@@ -20,10 +20,6 @@ setup(
     zip_safe=False,
     python_requires=">=3.5",
     classifiers=[
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.5",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
